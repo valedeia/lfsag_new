91ee041b7121bccc7a78f2464e6dfd7451355df0
valedeia <32706537+valedeia@users.noreply.github.com>
2017-12-05 16:42:45 +0100

    Add files via upload

---
+++ sito_new/js/contatti.js
@@ -0,0 +1,17 @@
+var sounds = {
+	"type": "FeatureCollection",
+	"features": [
+	{
+		"type": "Feature",
+		"properties": {
+			"name": "Laboratorio di Fonetica Sperimentale 'Arturo Genre'",
+			},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [7.69486,45.06790]
+		}
+	}
+]
+
+
+}
